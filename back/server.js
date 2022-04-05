96dec6185bfb3cffa08281feb4cfebfc3f4604f6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-05 15:03:17 +0200

    dev: express static

--- back/server.js
+++ back/server.js
@@ -5,15 +5,14 @@ const serveIndex = require("serve-index");
 
 const app = express();
 const port = 3000;
+const wwwDir = ".";
 
 app.use((req, res, next) => {
   console.log("req: ", req.url);
   next();
 });
 
-app.get("/*", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.static(wwwDir));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
