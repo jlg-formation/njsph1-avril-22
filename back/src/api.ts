92016f0c0acb1eb2f6fdf3426c82b4f17ebdbac6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-07 14:15:50 +0200

    dev: get ok

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,4 +1,10 @@
 import { Router } from "express";
+import { Article } from "./interfaces/Articles";
+
+const articles: Article[] = [
+  { id: "12", name: "Tondeuse", price: 120, qty: 9 },
+  { id: "12", name: "Marteau", price: 11, qty: 45 },
+];
 
 const app = Router();
 
@@ -12,6 +18,10 @@ app.get("/date", (req, res, next) => {
   res.json({
     date: new Date(),
   });
+});
+
+app.get("/articles", (req, res) => {
+  res.json(articles);
 });
 
 export const api = app;
