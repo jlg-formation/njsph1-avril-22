e3ca2ca368915a5196ddb4a1c50b50701bd5a805
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-04-08 15:13:28 +0200

    test: afterEach beforeEach

--- back/test/test.spec.ts
+++ back/test/test.spec.ts
@@ -1,13 +1,30 @@
 import assert from "assert";
 import { WebServer } from "../src/WebServer";
+import axios from "axios";
+
+process.env.PORT = "3232";
+
 describe("Server", function () {
-  it("should start and stop", async () => {
-    process.env.PORT = "3232";
-    const server = new WebServer();
-    server.on("webserver-on", () => {
-      console.log("webserver on");
-    });
+  const server = new WebServer();
+  server.on("webserver-on", () => {
+    console.log("webserver on");
+  });
+
+  beforeEach(async () => {
     await server.start();
+  });
+  afterEach(async () => {
     await server.stop();
+  });
+
+  it("should start and stop", async () => {
+    const result = await axios.get("http://localhost:3232/api/date");
+    console.log("result: ", result.data);
+    assert.ok(result.data.date);
+  });
+  it("should start and stop", async () => {
+    const result = await axios.get("http://localhost:3232/api/date");
+    console.log("result: ", result.data);
+    assert.ok(result.data.date);
   });
 });
